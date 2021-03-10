5f7f3149ca3c3492fe95544045231f3696c0d09d
dheyho <dheyho@mail.ru>
2021-03-10 17:07:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0;
+  }
+  let minValue = array[0];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] < minValue){
+      minValue = array[i];
+    }
+  }
+
+    return minValue;
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0;
+  }
+  let maxValue = 0;
+  for (let i = 0; i < array.length; i++){
+    if (array[i] > maxValue){
+      maxValue = array[i];
+    }
+  }
+  return maxValue;
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0;
+  }
+  let sum = 0;
+  let result = 0;
+  for (let i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  result = sum / array.length;
+  return result;
 }
